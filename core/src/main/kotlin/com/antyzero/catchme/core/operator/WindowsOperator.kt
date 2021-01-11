f05fd2e0d5e4e4e3c0190ac82bb167d4f94cdc0f
outlying <outlying@gmail.com>
2021-01-11 17:36:56 +0100

    Testing

--- core/src/main/kotlin/com/antyzero/catchme/core/operator/WindowsOperator.kt
+++ core/src/main/kotlin/com/antyzero/catchme/core/operator/WindowsOperator.kt
@@ -1,12 +1,11 @@
 package com.antyzero.catchme.core.operator
 
-import com.sun.jna.Native
-import com.sun.jna.win32.W32APIOptions.DEFAULT_OPTIONS
-import com.sun.jna.win32.W32APIOptions.DEFAULT_OPTIONS
+import java.io.BufferedReader
+import java.io.File
+import java.io.InputStreamReader
+import com.profesorfalken.jpowershell.PowerShell
 
-import com.sun.jna.win32.W32APIOptions
-
-
+import com.profesorfalken.jpowershell.PowerShellResponse
 
 
 
@@ -14,25 +13,81 @@ import com.sun.jna.win32.W32APIOptions
 
 object WindowsOperator : SharedOperator() {
 
+    private val runtime = Runtime.getRuntime()
+
+    private val focusScript = File.createTempFile("focus", ".js")
+    private val sizeScript = File.createTempFile("size", ".js")
+    private val positionScript = File.createTempFile("position", ".js")
+
+    init {
+        focusScript.writeText("(new ActiveXObject(\"WScript.Shell\")).AppActivate(\"Discord\"); ")
+
+        sizeScript.writeText("""
+            Wscript.Echo "Like this?"
+        """.trimIndent())
+
+        //positionScript.writeText("""""".trimIndent())
+
+        positionScript.writeText(sizeScript.readText())
+    }
+
     override suspend fun focusWindow() {
+        try {
+            runScriptFile(focusScript)
+        } catch (e: Exception) {
+            throw IllegalStateException("Unable to focus window", e)
+        }
     }
 
     override fun windowSize(): Pair<Int, Int> {
-        TODO("Not yet implemented")
+        try {
+            println(runScriptFile(focusScript))
+            return 0 to 0
+        } catch (e: Exception) {
+            throw IllegalStateException("Unable to focus window", e)
+        }
     }
 
     override fun windowPosition(): Pair<Int, Int> {
-        TODO("Not yet implemented")
-    }
 
-    interface User32 : W32APIOptions {
+        val response = PowerShell.executeSingleCommand("Get-Process -ProcessName Wow").commandOutput
 
-        companion object {
-            val instance = Native.loadLibrary(
-                "user32",
-                User32::class.java, DEFAULT_OPTIONS
-            ) as User32
-            const val SW_SHOW = 1
+        print(response)
+
+        return 0 to 0
+
+        try {
+            println(runScriptFile(focusScript))
+            return 0 to 0
+        } catch (e: Exception) {
+            throw IllegalStateException("Unable to focus window", e)
         }
     }
+
+    private fun runScriptFile(scriptFile: File): List<String> {
+        val process = runtime.exec("cscript //nologo //B ${scriptFile.absoluteFile}")
+
+        val stdInput = BufferedReader(InputStreamReader(process.inputStream))
+        val stdError = BufferedReader(InputStreamReader(process.errorStream))
+
+        val output = mutableListOf<String>()
+        val error = mutableListOf<String>()
+        var line: String?
+        while (!stdInput.readLine().also { line = it }.isNullOrBlank()) {
+            output.add(line!!)
+        }
+
+        while (!stdError.readLine().also { line = it }.isNullOrBlank()) {
+            error.add(line!!)
+        }
+
+        if (error.isNotEmpty()) {
+            throw RuntimeException("Script [${scriptFile.readText()}] execution error $error")
+        }
+
+        return output
+    }
+
+    private const val sizePositionScript = """
+    """
 }
